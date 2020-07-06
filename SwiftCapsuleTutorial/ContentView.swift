2b36c768318977265e0fb11e6f1117a189c13a92
Self Designs LLC <selfdesignsll@gmail.com>
2020-07-06 12:35:15 -0600

    Initial Commit

---
+++ SwiftCapsuleTutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftCapsuleTutorial
+//
+//  Created by Christopher Murray on 7/6/20.
+//  Copyright © 2020 Self Designs LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
